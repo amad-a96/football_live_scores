a4feb61bf347ab0a66051c4ab9347bba5d865a47
amad <amad.atroshi96@gmail.com>
2022-02-26 23:54:50 +0300

    changed future to stream

--- lib/services/football_Api_Manager.dart
+++ lib/services/football_Api_Manager.dart
@@ -6,16 +6,17 @@ import 'package:football_live_scores/models/Standings_Info.dart';
 import 'package:http/http.dart' as http;
 
 class FootballApiManager {
-  Future<LeaguesInfo> getResultsandFixtures() async {
+  Stream<LeaguesInfo> getResultsandFixtures(
+      List<DateTime> fromTo, String league_Id) async* {
     var matchs = null;
     String action = 'get_events';
-    String from = 'from=2022-02-16';
-    String to = 'to=2022-02-17';
-    String league_Id = '152';
+    String from = fromTo.first.toString();
+    String to = fromTo.last.toString();
+    //String league_Id = '152';
     String apiKey =
         'APIkey=214b70b8f9df90e70973320ca1a0ea45585ba9a0d969bd74cfd4a25f75e842ea';
     var url = Uri.parse(
-        'https://apiv3.apifootball.com/?action=get_events&from=2022-02-19&to=2022-02-20&league_id=$league_Id&APIkey=214b70b8f9df90e70973320ca1a0ea45585ba9a0d969bd74cfd4a25f75e842ea');
+        'https://apiv3.apifootball.com/?action=get_events&from=${fromTo.first}&to=${fromTo.last}&league_id=$league_Id&APIkey=214b70b8f9df90e70973320ca1a0ea45585ba9a0d969bd74cfd4a25f75e842ea');
     var client = http.Client();
 
     try {
@@ -29,23 +30,23 @@ class FootballApiManager {
         print("--------------------------------");
         var matchs = LeaguesInfo.fromJson(jsonMap);
         print(matchs);
-        return matchs;
+        yield matchs;
       }
     } catch (e) {
       print(e);
       // print(competitions);
-      return matchs;
+      // yield* matchs;
     }
     print(matchs);
     print("+++++++++++++++++++");
-    return matchs;
+    //yield* matchs;
   }
 
-  Future<StandingsInfo> getStandings() async {
+  Stream<StandingsInfo> getStandings(String league_Id) async* {
     var standings = null;
     String action = 'get_standings';
 
-    String league_Id = '152';
+    //String league_Id = '152';
     String apiKey =
         'APIkey=214b70b8f9df90e70973320ca1a0ea45585ba9a0d969bd74cfd4a25f75e842ea';
     var url = Uri.parse(
@@ -63,15 +64,15 @@ class FootballApiManager {
         print("--------------------------------");
         var standings = StandingsInfo.fromJson(jsonMap);
         print(standings);
-        return standings;
+        yield standings;
       }
     } catch (e) {
       print(e);
       // print(competitions);
-      return standings;
+      // return standings;
     }
     print(standings);
     print("+++++++++++++++++++");
-    return standings;
+    // return standings;
   }
 }
